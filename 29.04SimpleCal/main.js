1a08019c031a30cf7d168d6080b825a6de1b0b19
raulshukurov143 <73cunzl@code.edu.az>
2023-04-30 14:14:47 +0400

    Simple Calculator

---
+++ 29.04SimpleCal/main.js
@@ -0,0 +1,42 @@
+let content = document.querySelector("#content");
+let value1 = document.querySelector("#value1");
+let value2 = document.querySelector("#value2");
+let addBtn = document.querySelector("#addBtn");
+let subsBtn = document.querySelector("#subsBtn");
+let multiBtn = document.querySelector("#multiBtn");
+let divisBtn = document.querySelector("#divisBtn");
+let resetBtn = document.querySelector("#resetBtn");
+
+addBtn.addEventListener("click", function () {
+  if (value1.value === "" || value2.value === "") {
+    alert("please fill in the value lines");
+  } else {
+    content.innerText = +value1.value + +value2.value;
+  }
+});
+subsBtn.addEventListener("click", function () {
+  if (value1.value === "" || Value2.value === "") {
+    alert("please fill in the value lines");
+  } else {
+    content.innerText = +value1.value - +value2.value;
+  }
+});
+multiBtn.addEventListener("click", function () {
+  if (value1.value === "" || value2.value === "") {
+    alert("please fill in the value lines");
+  } else {
+    content.innerText = +value1.value * +value2.value;
+  }
+});
+divisBtn.addEventListener("click", function () {
+  if (value1.value === "" || value2.value === "") {
+    alert("please fill in the value lines");
+  } else {
+    content.innerText = +value1.value / +value2.value;
+  }
+});
+resetBtn.addEventListener("click", function () {
+  content.innerText = 0;
+  value1.value = "";
+  value2.value = "";
+});
